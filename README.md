7aea8e58da80845c7d02c50c10201f3fb3e1270c
iMaeGoo <mail1st@qq.com>
2020-08-08 01:41:58 +0800

    doc: add usage

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Instruction
 
-这是根据 [xCss/Valine#336](https://github.com/xCss/Valine/issues/336) 的需求实现的魔改版本。
+这是根据 [xCss/Valine#336](https://github.com/xCss/Valine/issues/336) 的需求实现的魔改版本，基于 Valine.min.js 版本 1.4.14 修改。
 
 因 *发送请求中响应内容明文暴露评论者IP、邮箱等隐私内容*，博客主需要[手动更改字段权限](https://leancloud.cn/docs/data_security.html#hash723958571)，然而，如果设置 mail 客户端不可见，将不会显示评论者的 Gravatar，转为显示默认头像。
 
@@ -12,6 +12,31 @@
 
 1. 访问 LeanCloud 控制台 > 存储 > 结构化数据
 2. 选择 Comment Class，分别点击 mail 和 ip 相应字段的下拉菜单，选择编辑
-3. 勾选"客户端不可见"后，客户端发起查询的时候，返回的结果将不包含这个字段
-4. 使用脚本处理现有数据
-5. 替换博客的 Valine
+![](./img/QQ20200808-011418.png)
+3. 勾选"客户端不可见"后，客户端发起查询的时候，返回的结果将不包含这个字段<br>
+\* 即使用 AppKey 指定字段名查询，也不会返回字段值，足够安全。
+![](./img/QQ20200808-011605.png)
+4. 请同样确认 class 的权限配置
+![](./img/QQ20200808-012251.png)
+5. 克隆本仓库
+``` sh
+# 克隆
+git clone -b imaegoo/1.4.14 https://github.com/imaegoo/Valine.git valine
+cd valine
+# 安装依赖
+npm install
+```
+6. 打开 `migrate.js`，修改里面的 appId，appKey，masterKey，serverURL 为自己的
+![](./img/QQ20200808-013448.png)
+7. 执行脚本
+``` sh
+node migrate.js
+```
+![](./img/QQ20200808-013100.png)
+
+8. 替换博客的 `Valine.min.js` 为本仓库到 `valine.js`
+9. 测试一下，头像是否正常加载，API 是否没有返回 mail 和 ip
+
+## Difference
+
+如果你对魔改版感到不放心，可以[检查检查我都改了些啥](https://github.com/imaegoo/Valine/commit/87b56361a30fe2b806ebb56154559141ffca34cb)
---
+++ migrate.js
@@ -0,0 +1,51 @@
+const AV = require('leancloud-storage');
+const md5 = require('blueimp-md5');
+
+// LeanCloud 后台 -> 设置 -> 应用 Keys
+AV.init({
+  appId: "AppID",
+  appKey: "AppKey",
+  masterKey: "MasterKey",
+  serverURL: "服务器地址"
+});
+
+// 搞到超级管理员权限（用来更新数据）
+AV.Cloud.useMasterKey(true);
+
+// 新建查询
+const query = new AV.Query('Comment');
+
+// 记录修改了几条记录
+let migratedCount = 0;
+
+/**
+ * LeanCloud 限制每次查询最多返回 1000 条
+ * 默默地看了一下我的博客的评论数：191
+ * 不会有谁的博客有超过 1000 评论吧？？不会吧？？？
+ * 算了还是考虑一下这种情况吧 T A T
+ */
+query.count().then((count) => {
+  const pageSize = 100;
+  const pageCount = Math.ceil(count / pageSize);
+  for (let pageNo = 0; pageNo < pageCount; pageNo++) {
+    query.ascending('createdAt');
+    query.skip(pageNo * pageSize);
+    query.limit(pageSize);
+    query.find().then((comments) => {
+      for (const comment of comments) {
+        migratedCount++;
+        const mail = comment.get('mail');
+        if (mail) {
+          const mailMd5 = md5(mail);
+          comment.set('mailMd5', mailMd5);
+          console.log(`已处理：${migratedCount} 条，邮箱：${mail}，MD5：${mailMd5}`);
+        } else {
+          comment.set('mailMd5', '');
+          console.log(`已处理：${migratedCount} 条，是个匿名评论～`);
+        }
+      }
+      AV.Object.saveAll(comments);
+      console.log(`已保存：第 ${pageNo + 1} 页评论数据`);
+    });
+  }
+});
